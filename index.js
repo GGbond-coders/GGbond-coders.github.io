dcc32761be120637e31a59a60df8740a2804e5ff
GGbond-coders <1219416739@qq.com>
2024-08-16 19:47:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var S = {
         S.Drawing.init('.canvas');
         document.body.classList.add('body--ready');
             //想说什么
-            S.UI.simulate("祝彭欣欣|生日快乐哟|#time");
+            S.UI.simulate("祝彭欣欣|生日快乐哟");
             S.Drawing.loop(function () {
                 S.Shape.render();
             });
